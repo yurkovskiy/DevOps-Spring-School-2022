6fd0b61f479ebae62deb360b5ceb36fe458eb323
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2022-02-05 00:10:18 +0200

    updated

--- script_1.sh
+++ script_1.sh
@@ -3,8 +3,8 @@
 LRED='\033[1;31m'
 NC='\033[0m' # No Color
 
-echo "DevOps Autumn School 2021 [Script 1]"
-echo "(c) Yuriy V. Bezgachnyuk 2021"
+echo "DevOps Spring School 2022 [Script 1]"
+echo "(c) Yuriy V. Bezgachnyuk 2022"
 echo "------------------------------------"
 today=`date +%d%m%Y_%H%M`
 echo -e "${LRED} Hello $USER today is $today and your current path is $PWD"
